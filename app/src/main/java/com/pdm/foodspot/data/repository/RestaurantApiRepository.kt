18fa5b9a254bf506cd659df5e68b9c5da6aef8c5
recinos <00182122@uca.edu.sv>
2026-05-15 04:51:24 -0600

    interface fun override api repository

--- app/src/main/java/com/pdm/foodspot/data/repository/RestaurantApiRepository.kt
+++ app/src/main/java/com/pdm/foodspot/data/repository/RestaurantApiRepository.kt
@@ -1,2 +1,25 @@
 package com.pdm.foodspot.data.repository
 
+import com.pdm.foodspot.data.dummy.restaurantList
+import com.pdm.foodspot.data.model.Restaurant
+import kotlinx.coroutines.delay
+
+class RestaurantApiRepository : RestaurantRepository {
+    override suspend fun getRestaurants(): List<Restaurant> {
+        delay(2000)
+        return restaurantList
+    }
+
+    override suspend fun getRestaurantById(id: Int): Restaurant? {
+        delay(5000)
+        return restaurantList.find { it.id == id }
+    }
+
+    override suspend fun searchRestaurants(query: String): List<Restaurant> {
+        return restaurantList.filter { restaurant ->
+            restaurant.name.contains(query, ignoreCase = true) ||
+                    restaurant.menu.any { it.name.contains(query, ignoreCase = true) }
+        }
+    }
+
+}
