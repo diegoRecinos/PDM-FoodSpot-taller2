ff8f09c64e5ce1d71bfdeeef2d9ac2b8e1c989da
recinos <00182122@uca.edu.sv>
2026-05-15 04:49:57 -0600

    interface

--- app/src/main/java/com/pdm/foodspot/data/repository/RestaurantRepository.kt
+++ app/src/main/java/com/pdm/foodspot/data/repository/RestaurantRepository.kt
@@ -1,2 +1,9 @@
 package com.pdm.foodspot.data.repository
 
+import com.pdm.foodspot.data.model.Restaurant
+
+interface RestaurantRepository {
+    suspend fun getRestaurants(): List<Restaurant>
+    suspend fun getRestaurantById(id: Int): Restaurant?
+    suspend fun searchRestaurants(query: String): List<Restaurant>
+}
